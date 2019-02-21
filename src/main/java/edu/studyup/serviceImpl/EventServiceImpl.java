55518e43917fd0f4846bd17904771389c4f8b344
Jonathan Cheng <jonathancheng@campus-039-036.ucdavis.edu>
2019-02-20 16:56:22 -0800

    HW#3: eventServiceImpl

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import edu.studyup.entity.Event;
 import edu.studyup.entity.Student;
@@ -34,9 +35,11 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> activeEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
-			activeEvents.add(ithEvent);
+		for (Entry<Integer, Event> event : eventData.entrySet()) {
+			Event ithEvent = event.getValue();
+			if (ithEvent.getDate().equals(new Date()) || ithEvent.getDate().after(new Date())) {
+				activeEvents.add(ithEvent);
+			}
 		}
 		return activeEvents;
 	}
@@ -46,8 +49,8 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> pastEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
+		for (Entry<Integer, Event> event : eventData.entrySet()) {
+			Event ithEvent= event.getValue();
 			// Checks if an event date is before today, if yes, then add to the past event list.
 			if(ithEvent.getDate().before(new Date())) {
 				pastEvents.add(ithEvent);
